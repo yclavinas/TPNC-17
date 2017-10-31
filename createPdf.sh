e5ca5ea0f65e46b4409d57dd4838e57181ab5ab9
Yuri Lavinas <yurilavinas@Yuris-MacBook-Pro.local>
2017-10-31 12:02:48 +0900

    final version

--- createPdf.sh
+++ createPdf.sh
@@ -1,4 +1,4 @@
-platex -src -interaction=nonstopmode main.tex img/pressure-1 img/pressure-21 img/unnamed-chunk-1-1 img/unnamed-chunk-1-11
-dvipdf main.dvi
-open main.pdf
-
+platex main.tex
+dvipdfmx main.dvi
+mv main.pdf ExperimentalAnalysisoftheTournamentSizeonGA.pdf
+open ExperimentalAnalysisoftheTournamentSizeonGA.pdf
